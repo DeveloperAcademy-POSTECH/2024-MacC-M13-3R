5f6a1f9c2814e46dad5562fd98c3f891ced9a6a5
Winterrr-24 <dbwls9924@naver.com>
2024-10-15 11:04:07 +0900

    [Design] #38 - 예산뷰 수정 및 기능구현 (지도실패)

--- TestProject-3R/Source/View/BudgetView.swift
+++ TestProject-3R/Source/View/BudgetView.swift
@@ -7,7 +7,19 @@
 
 import SwiftUI
 
+
 struct BudgetView: View {
+    @State var sliderValue = 0.0
+    //    @State var inputValue = "50000"
+    @State var showSheet = false
+    @FocusState private var isTextFieldFocused: Bool
+    //    @State private var locationDataManager = LocationManager()
+    @State private var latitude: Double = 0
+    @State private var longitude: Double = 0
+    @State private var cityN:String = ""
+    @State  private var countryC:String = ""
+    
+    
     var body: some View {
         NavigationStack {
             VStack(alignment: .center) {
@@ -21,23 +33,48 @@ struct BudgetView: View {
                 Divider()
                     .padding(.bottom, 16)
                 HStack{
-                    Text("오늘의 장보기 예산을 정해주세요")
+                    Text("오늘의 장보기 예산을 입력해주세요")
                         .font(.system(size: 12, weight: .medium))
                     Spacer()
                 }
                 .padding(.bottom, 2)
+                
                 HStack(alignment: .bottom){
-                    Text("50,000원")
+                    Text("\(Int(sliderValue)) 원")
                         .font(.system(size: 18))
-                    Text("직접입력")
-                        .font(.system(size: 8, weight: .medium))
-                        .underline()
-                        .foregroundColor(.gray)
+                    
+                    Button("직접입력") {
+                        showSheet = true
+                    }
+                    .font(.system(size: 11, weight: .medium))
+                    .underline()
+                    .foregroundColor(.gray)
+                    .sheet(isPresented: $showSheet) {
+                        ModalView(showSheet: $showSheet, sliderValue: $sliderValue)
+                            .presentationDetents([.height(300)])
+                            .presentationDragIndicator(.visible)
+                    }
                     Spacer()
                 }
                 .padding(.bottom, 20)
-                Image("sample_slider")
-                    .padding(.bottom, 54)
+                
+                Slider(value: $sliderValue, in: 50000 ... 250000, step: 10000)
+                    .frame(width: 328)
+                    .onChange(of: sliderValue) { NewValue in
+                        let impactMed = UIImpactFeedbackGenerator(style: .light)
+                        impactMed.impactOccurred()
+                    }
+                HStack {
+                    Text("50,000-")
+                    Spacer()
+                    Text("250,000+")
+                }
+                .font(.caption)
+                .foregroundColor(.gray)
+                .padding(.horizontal)
+                
+                .padding(.bottom, 54)
+                
                 
                 HStack{
                     Text("장소 입력하기")
@@ -63,9 +100,17 @@ struct BudgetView: View {
                     Spacer()
                 }
                 .padding(.bottom, 20)
+                
+                //                Text("City : \(cityN)")
+                //                    .font(.system(size: 28, weight: .bold))
+                //                Text("Latitude : \(latitude)")
+                //                    .font(.system(size: 28, weight: .bold))
+                //                Text("Longitude : \(longitude)")
+                //                    .font(.system(size: 28, weight: .bold))
                 Image("sample_map")
+                
                 Spacer()
-                NavigationLink(destination: CartView()) {
+                NavigationLink(destination: UpdateView(speechRecognizer: SpeechRecognizer())) {
                     Text("장보기 시작하기")
                         .fontWeight(.semibold)
                         .foregroundColor(.white)
@@ -76,14 +121,93 @@ struct BudgetView: View {
                         .cornerRadius(15)
                         .padding(.bottom, 36)
                 }
-                
             }
             .padding(.horizontal, 16)
             .navigationTitle("장보기 준비하기")
             .navigationBarTitleDisplayMode(.inline)
         }
         .tint(.green)
+        .onAppear(){
+            //            let yourLatitudeString = String(locationDataManager.locationManager.location?.coordinate.latitude.description ?? "Error loading")
+            //            let yourLongitudeString = String(locationDataManager.locationManager.location?.coordinate.longitude.description ?? "Error loading")
+            //            self.latitude = (Double(yourLatitudeString) ?? 0)
+            //            self.longitude =  (Double(yourLongitudeString) ?? 0)
+            //            // city and country
+            //            let locationInfo = LocationInfo()
+            //            locationInfo.getCityLocation(latitude: self.latitude, longitude: self.longitude) { city, country in
+            //                if let city = city, let country = country {
+            //                    self.cityN = city
+            //                    self.countryC = country
+            //                    print("City: \(city), Country: \(country)")
+            //                } else {
+            //                    print("Unable to fetch location information.")
+            //                }
+            //            }
+        }
     }
+    //    private func updateSliderValue() {
+    //        if let newDoubleValue = Double(sliderValue) {
+    //            DispatchQueue.main.async{
+    //
+    //                sliderValue = newDoubleValue
+    //            }
+    //        }
+    //    }
+}
+
+struct ModalView: View {
+    @State private var inputValue = "50000"
+    @Binding var showSheet: Bool
+    @Binding var sliderValue: Double
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("오늘의 장보기 예산을 입력해주세요")
+                .font(.system(size: 15, weight: .medium))
+            Spacer()
+            HStack {
+                TextField("50000", text: $inputValue)
+                    .font(.system(size: 30, weight: .bold))
+                    .keyboardType(.decimalPad)
+                    .font(.system(size: 16, weight: .medium))
+//                    .onSubmit {
+//                        if let newValue = Double(inputValue){
+//                            sliderValue = newValue
+//                        }
+//                        showSheet = false
+//                    }
+                    .frame(width: 200)
+                    .multilineTextAlignment(.center)
+                Text("원")
+                    .font(.system(size: 20, weight: .medium))
+            }
+            Spacer()
+            Button("입력") {
+                if let newValue = Double(inputValue){
+                    sliderValue = newValue
+                }
+                showSheet = false
+            }
+            .fontWeight(.semibold)
+            .foregroundColor(.white)
+            .font(.system(size: 16))
+            .padding(.horizontal, 132)
+            .padding(.vertical, 12)
+            .background(.green)
+            .cornerRadius(15)
+            .padding(.bottom, 36)
+        }
+        .onAppear {
+            inputValue = "\(Int(sliderValue))"
+        }
+        
+    }
+}
+
+func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+    let generator = UIImpactFeedbackGenerator(style: style)
+    generator.impactOccurred()
 }
 
 #Preview {
